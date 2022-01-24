45c383834a26c8af156e200b0011c7fc44c64bfc
Matt Soucy <git@msoucy.me>
2022-01-24 17:21:32 -0500

    Badges profile page

--- signinapp/user.py
+++ signinapp/user.py
@@ -11,7 +11,7 @@ from flask_wtf import FlaskForm
 from wtforms import PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
-from .model import EventType, Person
+from .model import Badge, EventType, Person
 
 user = Blueprint("user", __name__)
 
@@ -28,4 +28,17 @@ def profile():
     if current_user.can_view(user):
         event_types = EventType.query.all()
         return render_template("profile.html.jinja2", user=user, event_types=event_types)
+    return current_app.login_manager.unauthorized()
+
+
+@user.route("/badge")
+@login_required
+def badge():
+    if bid := request.args.get("badge_id"):
+        bid = int(bid)
+        badge = Badge.query.get(bid)
+        owners = [p for p in Person.query.filter_by(active=True).all()
+                  if p.has_badge(bid)]
+        print(bid, owners)
+        return render_template("badge.html.jinja2", badge=badge, owners=owners)
     return current_app.login_manager.unauthorized()
