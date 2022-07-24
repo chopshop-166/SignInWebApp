242af0296a61812d4d9710ae1a06cf2f77684637
Matt Soucy <git@msoucy.me>
2022-07-24 11:15:57 -0400

    Move user view privs to mentor

--- signinapp/team.py
+++ signinapp/team.py
@@ -1,10 +1,18 @@
 from flask import Blueprint, request
 from flask.templating import render_template
-from flask_login import login_required
+from flask_login import current_user, login_required
 
-from .model import Subteam
+from .model import Role, Subteam, User
 
 team = Blueprint("team", __name__)
+
+
+@team.route("/admin/users")
+@login_required
+def users():
+    users = User.query.all()
+    roles = Role.query.all()
+    return render_template("users.html.jinja2", users=users, roles=roles)
 
 
 @team.route("/subteam")
