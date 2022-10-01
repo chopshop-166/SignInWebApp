63021b12552087ddd820e86aeb67ae5664ad67ef
Joe Janicki <joejanicki26@gmail.com>
2022-10-01 14:30:34 -0400

    Use url_for when generating QR codes

--- signinapp/qr.py
+++ signinapp/qr.py
@@ -9,16 +9,22 @@ qr = Blueprint("qr", __name__)
 @qr.route("/register/qr")
 def register_qr():
     url = request.host_url
-    return render_template("qr.html.jinja2", register_url=f"{url}register")
+    return render_template(
+        "qr.html.jinja2", register_url=f"{url}{url_for('auth.register')}"
+    )
 
 
 @qr.route("/register/mentor/qr")
 def register_mentor_qr():
     url = request.host_url
-    return render_template("qr.html.jinja2", register_url=f"{url}register/mentor")
+    return render_template(
+        "qr.html.jinja2", register_url=f"{url}{url_for('auth.register_mentor')}"
+    )
 
 
 @qr.route("/register/guardian/qr")
 def register_guardian_qr():
     url = request.host_url
-    return render_template("qr.html.jinja2", register_url=f"{url}register/guardian")
+    return render_template(
+        "qr.html.jinja2", register_url=f"{url}{url_for('auth.register_guardian')}"
+    )
