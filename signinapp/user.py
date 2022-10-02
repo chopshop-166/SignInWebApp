48707f525ab8e21c0fe2a7a9eba1149b149b95dd
Matt Soucy <git@msoucy.me>
2022-10-02 12:06:42 -0400

    User profile convenience link cleanup

--- signinapp/user.py
+++ signinapp/user.py
@@ -8,33 +8,24 @@ from .model import Badge, EventType, User, db
 user = Blueprint("user", __name__)
 
 
-@user.route("/profile")
+@user.route("/profile/")
 @login_required
-def profile():
-    user = current_user
-    if uid := request.args.get("user_id"):
-        user = db.session.get(User, uid)
-    if current_user.can_view(user):
-        event_types = db.session.scalars(select(EventType))
-        return render_template(
-            "profile.html.jinja2", user=user, event_types=event_types
-        )
-    return current_app.login_manager.unauthorized()
+def profile_self():
+    return redirect(url_for("user.profile", username=current_user.username))
 
 
 @user.route("/profile/<username>")
 @login_required
-def profile_url(username):
+def profile(username):
     user = db.session.scalar(select(User).filter_by(username=username))
     if not user:
-        flash("Invalid user")
+        flash("Invalid user for profile")
         return redirect(url_for("index"))
-    if current_user.can_view(user):
-        event_types = db.session.scalars(select(EventType))
-        return render_template(
-            "profile.html.jinja2", user=user, event_types=event_types
-        )
-    return current_app.login_manager.unauthorized()
+    if not current_user.can_view(user):
+        flash(f"No acces to view user data for {user.name}")
+        return redirect(url_for("index"))
+    event_types = db.session.scalars(select(EventType))
+    return render_template("profile.html.jinja2", user=user, event_types=event_types)
 
 
 @user.route("/badge")
