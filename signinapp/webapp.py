89dd0c404dc6fe973accb325a35abe66e6b5471a
Matt Soucy <git@msoucy.me>
2022-10-04 23:27:20 -0400

    Add gen-codes cli option

--- signinapp/webapp.py
+++ signinapp/webapp.py
@@ -1,9 +1,10 @@
 # Entry point for the application.
 import click
 from flask.cli import with_appcontext
+from sqlalchemy.future import select
 
 # For application discovery by the 'flask' command.
-from . import app, init_default_db
+from . import app, init_default_db, db
 
 # For import side-effects of setting up routes.
 from . import auth, event, model, team, user
@@ -20,7 +21,23 @@ def init_db_command():
     click.echo("Initialized the database.")
 
 
+@click.command("gen-codes")
+@with_appcontext
+def gen_codes_command():
+    """Generate user codes for all users."""
+
+    for user in db.session.scalars(
+        select(model.User)
+    ):
+        user.code = model.gen_code()
+
+    db.session.commit()
+
+    click.echo("Generated new user codes for all users.")
+
+
 app.cli.add_command(init_db_command)
+app.cli.add_command(gen_codes_command)
 
 if __name__ == "__main__":
     app.run()
