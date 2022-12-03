7bf53eb90a562cbb489815a2b6db6135f80214d9
Matt Soucy <git@msoucy.me>
2022-12-03 18:44:14 -0500

    Fix badge search
    Do filtering in python

--- signinapp/search.py
+++ signinapp/search.py
@@ -28,15 +28,15 @@ def badges():
     form = BadgeSearchForm()
 
     if form.validate_on_submit():
-        stmt = User.get_visible_users()
+        users = User.get_visible_users()
         if int(form.subteam.data) != 0:
-            stmt = stmt.filter_by(subteam_id=form.subteam.data)
-        results = db.session.scalars(stmt)
+            subteam = db.session.get(Subteam, form.subteam.data)
+            users = [u for u in users if u.subteam == subteam]
         badge = db.session.get(Badge, form.badge.data)
         if form.required.data:
-            results = [u for u in results if badge in u.badges]
+            results = [u for u in users if badge in u.badges]
         else:
-            results = [u for u in results if badge not in u.badges]
+            results = [u for u in users if badge not in u.badges]
         return render_template("search/badges.html.jinja2", form=form, results=results)
     return render_template("search/badges.html.jinja2", form=form, results=None)
 
@@ -48,13 +48,13 @@ def hours():
     form.role.choices = [r.name for r in Role.get_visible()]
 
     if form.validate_on_submit():
-        results = User.get_visible_users()
+        users = User.get_visible_users()
         event_type = db.session.get(EventType, form.category.data)
         roles = [Role.from_name(r).id for r in form.role.data]
         results = sorted(
             [
                 (u.display_name, u.total_stamps_for(event_type))
-                for u in results
+                for u in users
                 if u.role_id in roles and u.total_time and u.approved
             ]
         )
